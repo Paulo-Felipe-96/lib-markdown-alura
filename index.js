43f7fd07c6b3459c9187fe91fd9fee0ab929cc35
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-06-02 18:03:54 -0300

    feat: third way read file async await

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ function trataErro(error) {
   throw new Error(chalk.redBright(error));
 }
 
-function pegaArquivo(path) {
-  fs.promises
-    .readFile(path, "utf-8")
-    .then((data) => console.log(chalk.green(data)))
-    .catch((error) => trataErro(error));
+async function pegaArquivo(path) {
+  try {
+    const data = await fs.promises.readFile(path, "utf-8");
+    console.log(chalk.green(data));
+  } catch (e) {
+    trataErro(e);
+  }
 }
 
 pegaArquivo("./texts.md");
